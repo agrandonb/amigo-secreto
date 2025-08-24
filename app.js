d2bdd2216e22b65334c79831f2003e1d1e2a5fbf
agrandonb <agrandonbello@gmail.com>
2025-08-24 15:39:35 -0400

    primera parte del codigo creado por mi, falta revisar

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+let textoPrincipal = document.querySelector("h2");
+let lista = document.getElementById("listaAmigos");
+
+function condicionInicial() {
+    textoPrincipal;
+    document.getElementById("amigo").value = "";
+}
+
+function agregarAmigos() {
+        let nombre = document.getElementById("amigo").value;
+
+    if (nombre === "") {
+        textoPrincipal.innerText = "Por favor, inserte un nombre";
+    } else {
+        nombresAmigos.push(nombre);
+        textoPrincipal.innerText = "Amigo agregado correctamente";
+        actualizarLista();
+        condicionInicial();
+    }
+}
+
+function actualizarLista () {
+    listaAmigos.innerHTML = "";
+        
+      for (let i = 0; i < nombresAmigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = nombresAmigos[i];
+        lista.appendChild(li);
+    }
+}
+
